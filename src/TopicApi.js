d62fdf1b8be75f421a62a6d369cd6ce0b0c66555
IndianAtomsDesignTeam <165372005+IndianAtomsDesignTeam@users.noreply.github.com>
2024-07-29 19:52:44 +0530

    Add files via upload

---
+++ src/TopicApi.js
@@ -0,0 +1,146 @@
+const Topic = [
+    {
+      id: 1,
+      clas:6,
+      image: "image/1.svg",
+      name: "Knowing Our Numbers",
+      linkQuizstart:"./QuizStartPage",
+
+    },
+    {
+      id: 2,
+      clas:6,
+      image: "image/2.svg",
+      name: "Whole Numbers",
+
+    },
+    {
+      id: 3,
+      clas:6,
+      image: "image/3.svg",
+      name: "Playing with Numbers",
+
+    },
+    {
+      id: 4,
+      clas:6,
+      image: "image/4.svg",
+      name: "Basic Geometrical Ideas",
+
+    },
+    {
+      id: 5,
+      clas:6,
+      image: "image/5.svg",
+      name: "Understanding Elementary Shapes",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 6,
+      clas:6,
+      image: "image/6.svg",
+      name: "Integers",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 7,
+      clas:6,
+      image: "image/7.svg",
+      name: "Fractions",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 8,
+      clas:6,
+      image: "image/8.svg",
+      name: "Plant & Nutrations",
+      // category: "Decimals",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      clas:6,
+      image: "image/9.svg",
+      name: "Data Handling",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      clas:6,
+      image: "image/10.svg",
+      name: "Mensuration",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 11,
+      clas:6,
+      image: "image/11.svg",
+      name: "Algebra",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      clas:6,
+      image: "image/12.svg",
+      name: "Ratio and Proportion",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 13,
+      clas:6,
+      image: "image/13.svg",
+      name: "Symmetry",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 14,
+      clas:7,
+      image: "image/14.svg",
+      name: "Practical Geometry",
+      // category: "breakfast",
+      // price: "12₹",
+      // description:
+      //   "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    // {
+    //   id: 8,
+    //   image: "image/12.svg",
+    //   name: "Plant & Nutrations",
+    //   category: "breakfast",
+    //   price: "12₹",
+    //   description:
+    //     "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    // },
+  
+
+  ];
+  
+  export default Topic;
